b1dce2c0dcb053e4a79098ea41ac90fcdd0b75ec
Timothée Mazzucotelli <dev@pawamoy.fr>
2024-05-22 18:41:20 +0200

    feat: Implement C extractor
    Co-authored-by: ZeroIntensity <zintensitydev@gmail.com>

--- src/mkdocstrings_handlers/c/handler.py
+++ src/mkdocstrings_handlers/c/handler.py
@@ -2,17 +2,450 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, ClassVar, Mapping, MutableMapping
+import re
+from dataclasses import dataclass
+from enum import Enum
+from io import StringIO
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, ClassVar, Mapping, MutableMapping, Protocol
 
-from mkdocs.exceptions import PluginError
 from mkdocstrings.handlers.base import BaseHandler, CollectionError, CollectorItem
 from mkdocstrings.loggers import get_logger
+from pycparser import CParser, c_ast
 
 if TYPE_CHECKING:
     from markdown import Markdown
+    from pycparser.c_ast import FileAST
 
 
 logger = get_logger(__name__)
+
+
+@dataclass
+class Comment:
+    """A comment extracted from the source code."""
+
+    text: str
+    last_line_number: int
+
+
+@dataclass
+class Macro:
+    """A macro extracted from the source code."""
+
+    text: str
+    line_number: int
+
+
+_C_PARSER = CParser()
+_END_COMMENT = re.compile(r'^(?!").+; *((\/\/.*)|(\/\*.*\*\/))$')
+_END_COMMENT_MACRO = re.compile(r"#.+ *((\/\/.*)|(\/\*.*\*\/))$")
+_SINGLE_COMMENT = re.compile(r" *\/\/ ?(.*)")
+_SINGLE_COMMENT_ALT = re.compile(r"\/\* *(.+) *\*\/")
+_FULL_DOC = re.compile(r"\/\*!?\n((.|\n)+)\n *\*\/")
+_DESC = re.compile(r" *\* *(.+)")
+_DIRECTIVE = re.compile(r" *\* *@(\w+)(\[.+\])? *(.+)")
+_PARAM_BODY = re.compile(r"(\w+) *(.+)")
+_DEFINE = re.compile(r"# *define (\w+) *(\w*)")
+
+
+def extract_comments(code: str) -> tuple[list[Comment], str]:
+    """Extract comments from the source code.
+
+    Parameters:
+        code: The source code to extract comments from.
+
+    Returns:
+        A tuple containing a list of comments and the source code with comments removed.
+    """
+    comments: list[Comment] = []
+    extracted: list[str] = []
+    in_comment: bool = False
+    buffer = StringIO()
+
+    for index, line in enumerate(code.split("\n")):
+        content = line.lstrip(" ").rstrip(" ")
+        if content.startswith("//") or (content.startswith("/*") and content.endswith("*/")):
+            # single line comment
+            comments.append(Comment(content, index + 1))
+            extracted.append("")  # preserve line count
+        elif match := _END_COMMENT_MACRO.match(line):
+            # comment at end of preprocessor directive
+            comments.append(Comment(match.group(2), index + 1))
+            extracted.append(line[: match.start(2)])
+            continue
+        elif match := _END_COMMENT.match(line):
+            # comment at end of line
+            comments.append(Comment(match.group(1), index + 1))
+            extracted.append(line[: match.start(1)])
+            continue
+        elif content.startswith("/*"):
+            # start of multiline comment
+            in_comment = True
+            buffer.write(content + "\n")
+        elif content.endswith("*/"):
+            # end of multiline comment
+            if not in_comment:
+                raise CollectionError("Found close to multiline comment without a start!")
+
+            in_comment = False
+            buffer.write(line)
+            bufval = buffer.getvalue()
+            comments.append(Comment(bufval, index + 1))
+            buffer.truncate(0)
+
+            for _ in range(bufval.count("\n") + 1):
+                extracted.append("")  # preserve line count
+        elif in_comment:
+            # we want to preserve the indentation
+            # here, so use line instead of content
+            buffer.write(line + "\n")
+        else:
+            # not a comment
+            extracted.append(line)
+
+    if in_comment:
+        raise CollectionError("Unterminated comment!")
+
+    return comments, "\n".join(extracted)
+
+
+def extract_macros(code: str) -> tuple[list[Macro], str]:
+    """Extract macros from the source code.
+
+    Parameters:
+        code: The source code to extract macros from.
+
+    Returns:
+        A tuple containing a list of macros and the source code with macros removed.
+    """
+    extracted: list[str] = []
+    macros: list[Macro] = []
+
+    # buffer variables
+    next_is_macro: bool = False
+    buffer = StringIO()
+    start_line = -1
+
+    for index, line in enumerate(code.split("\n")):
+        content = line.lstrip(" ").rstrip(" ")
+
+        if (not content.startswith("#")) and (not next_is_macro):
+            extracted.append(line)
+            continue
+
+        extracted.append("")
+
+        if next_is_macro:
+            next_is_macro = False
+            buffer.write("\n" + content)
+
+        if content.endswith("\\"):
+            if not next_is_macro:
+                # start of macro
+                start_line = index + 1
+                buffer.write(content)
+
+            next_is_macro = True
+
+        bufval = buffer.getvalue()
+        if (not next_is_macro) and (bufval):
+            # multiline macro has ended
+            macros.append(Macro(bufval, start_line))
+            start_line = -1
+            buffer.truncate(0)
+        else:
+            # single line macro
+            macros.append(Macro(content, index + 1))
+
+    return macros, "\n".join(extracted)
+
+
+class InOut(Enum):
+    """Enumeration for parameter direction."""
+
+    UNSPECIFIED = 0
+    IN = 1
+    OUT = 2
+
+
+@dataclass
+class Param:
+    """A parameter in a function signature."""
+
+    name: str
+    desc: str
+    in_out: InOut
+
+
+@dataclass
+class Docstring:
+    """A parsed docstring."""
+
+    desc: str
+    params: list[Param] | None = None
+    ret: str | None = None
+
+
+def parse_docstring(content: str) -> Docstring:
+    """Parse a docstring.
+
+    Parameters:
+        content: The content of the docstring.
+
+    Returns:
+        A parsed docstring.
+    """
+    single = _SINGLE_COMMENT.match(content)
+
+    if single:
+        return Docstring(single.group(1))
+
+    single_alt = _SINGLE_COMMENT_ALT.match(content)
+    if single_alt:
+        return Docstring(single_alt.group(1))
+
+    full = _FULL_DOC.match(content)
+    if not full:
+        raise CollectionError(f"Could not parse docstring! {content}")
+
+    text = full.group(1)
+    desc = StringIO()
+    split = text.split("\n")
+    start_index = -1
+    params: list[Param] = []
+    returns: str | None = None
+
+    for index, i in enumerate(split):
+        if "@" in i:
+            start_index = index
+            break
+
+        match = _DESC.match(i)
+        if not match:
+            raise CollectionError(f"Invalid docstring syntax: {i}")
+
+        desc.write(match.group(1) + " ")
+
+    for directive in split[start_index:]:
+        match = _DIRECTIVE.match(directive)
+
+        if not match:
+            raise CollectionError(f"Invalid docstring syntax: {directive}")
+
+        name = match.group(1)
+
+        if name == "param":
+            in_out_str = match.group(2)
+
+            if in_out_str == "[in]":
+                in_out = InOut.IN
+            elif in_out_str == "[out]":
+                in_out = InOut.OUT
+            else:
+                in_out = InOut.UNSPECIFIED
+
+            body = _PARAM_BODY.match(match.group(3))
+
+            if not body:
+                raise CollectionError(f"Invalid @param body: {body}")
+
+            name = body.group(1)
+            param_desc = body.group(2)
+            params.append(Param(name, param_desc, in_out))
+        elif name in {"return", "returns"}:
+            if returns:
+                raise CollectionError("Multiple @returns found!")
+            returns = match.group(3)
+        else:
+            raise CollectionError(f"Invalid directive in docstring: {name}")
+
+    return Docstring(desc.getvalue(), params, returns)
+
+
+@dataclass
+class DocMacro:
+    """A parsed macro."""
+
+    name: str
+    content: str | None
+    doc: Docstring | None
+
+
+@dataclass
+class DocType:
+    """A parsed typedef."""
+
+    name: str
+    tp: TypeRef
+    doc: Docstring | None
+    quals: list[str]
+
+
+@dataclass
+class DocGlobalVar:
+    """A parsed global variable."""
+
+    name: str
+    tp: TypeRef
+    doc: Docstring | None
+    quals: list[str]
+
+
+@dataclass
+class FuncParam:
+    """A parameter in a function signature."""
+
+    name: str
+    tp: TypeRef
+
+
+@dataclass
+class DocFunc:
+    """A parsed function."""
+
+    name: str
+    args: list[FuncParam]
+    ret: TypeRef
+    doc: Docstring | None
+
+
+@dataclass
+class CodeDoc:
+    """A parsed C source file."""
+
+    macros: list[DocMacro]
+    functions: list[DocFunc]
+    global_vars: list[DocGlobalVar]
+    typedefs: dict[str, DocType]
+
+
+class TypeDecl(Enum):
+    """Enumeration for type declarations."""
+
+    NORMAL = 0
+    POINTER = 1
+    ARRAY = 2
+    FUNCTION = 3
+
+
+@dataclass
+class TypeRef:
+    """A reference to a type in C."""
+
+    name: TypeRef | str
+    decl: TypeDecl
+    quals: list[str]
+    params: list[TypeRef] | None = None  # only in functions
+
+
+class SupportsQualsAndType(Protocol):
+    """A protocol for types that can have qualifiers and a type."""
+
+    quals: list[str]
+    type: SupportsQualsAndType | c_ast.TypeDecl | c_ast.IdentifierType
+
+
+def ast_to_decl(node: SupportsQualsAndType, types: dict[str, DocType]) -> TypeRef:
+    """Convert a pycparser AST node to a TypeRef."""
+    if isinstance(node, c_ast.TypeDecl):
+        # assert isinstance(node.type, c_ast.IdentifierType)
+        name = node.type.names[0]
+        existing = types.get(name)
+
+        if existing:
+            return existing.tp
+
+        return TypeRef(name, TypeDecl.NORMAL, node.quals)
+
+    if isinstance(node, c_ast.PtrDecl):
+        # assert not isinstance(node.type, c_ast.IdentifierType)
+        return TypeRef(ast_to_decl(node.type, types), TypeDecl.POINTER, node.quals)
+
+    if isinstance(node, c_ast.ArrayDecl):
+        return TypeRef(ast_to_decl(node.type, types), TypeDecl.ARRAY, node.quals)
+
+    # assert isinstance(node, c_ast.FuncDecl), f"expected a FuncDecl, got {node}"
+    return TypeRef(
+        ast_to_decl(node.type, types),
+        TypeDecl.FUNCTION,
+        [],
+        [ast_to_decl(decl.type, types) for decl in node.args.params],  # type: ignore[attr-defined]
+    )
+
+
+def _tp_ref_format_char(ref: TypeRef, char: str, qualname: str) -> str:
+    # assert isinstance(ref.name, TypeRef)
+    content = tp_ref_to_str(ref.name, qualname)  # type: ignore[arg-type]
+    if ref.quals:
+        return f"({' '.join(ref.quals)} {content}{char})"
+    return f"{content}{char}"
+
+
+def tp_ref_to_str(ref: TypeRef, qualname: str) -> str:
+    """Convert a TypeRef to a string.
+
+    Parameters:
+        ref: The TypeRef to convert.
+        qualname: The name of the type.
+
+    Returns:
+        The string representation of the TypeRef.
+    """
+    if ref.decl == TypeDecl.NORMAL:
+        if ref.quals:
+            return f"{' '.join(ref.quals)} {ref.name}"
+
+        return ref.name  # type: ignore[return-value]
+
+    if ref.decl == TypeDecl.POINTER:
+        return _tp_ref_format_char(ref, "*", qualname)
+
+    if ref.decl == TypeDecl.ARRAY:
+        return _tp_ref_format_char(ref, "[]", qualname)
+
+    # assert ref.decl == TypeDecl.FUNCTION
+    # assert ref.params is not None
+
+    params: list[str] = [tp_ref_to_str(i, qualname) for i in ref.params]  # type: ignore[union-attr]
+    ret = tp_ref_to_str(ref.name, qualname) if isinstance(ref.name, TypeRef) else ref.name
+
+    return f"{ret} (*{qualname})({', '.join(params)})"
+
+
+def desc(doc: Docstring | None) -> str:
+    """Get the description from a docstring.
+
+    Parameters:
+        doc: The docstring to get the description from.
+
+    Returns:
+        The description.
+    """
+    if not doc:
+        return "No description specified."
+
+    return doc.desc
+
+
+def lookup_type_html(data: CodeDoc, tp: TypeRef, *, name: str | None = None) -> str:
+    """Lookup a type and return an HTML representation.
+
+    Parameters:
+        data: The parsed C source file.
+        tp: The type to lookup.
+        name: The name of the type.
+
+    Returns:
+        The HTML representation of the type.
+    """
+    tp_str = ""
+
+    for name, doctype in data.typedefs.items():
+        if doctype.tp == tp:
+            tp_str = f'<a href="#type-{name}">{name}</a>'
+
+    return f'<code>{tp_str or tp_ref_to_str(tp, name or "unknown")}</code>'
 
 
 class CHandler(BaseHandler):
@@ -60,9 +493,84 @@ class CHandler(BaseHandler):
         Returns:
             Anything you want, as long as you can feed it to the `render` method.
         """
-        raise CollectionError("Implement me!")
+        source = Path(identifier).read_text(encoding="utf-8")
+        comments_list, source = extract_comments(source)
+        macros_list, source = extract_macros(source)
+        code: FileAST = _C_PARSER.parse(source)
 
-    def render(self, data: CollectorItem, config: Mapping[str, Any]) -> str:  # noqa: ARG002
+        comments: dict[int, Comment] = {comment.last_line_number: comment for comment in comments_list}
+        types: dict[str, DocType] = {}
+        global_vars: list[DocGlobalVar] = []
+        funcs: list[DocFunc] = []
+
+        for node in code.ext:
+            if not isinstance(node, (c_ast.Typedef, c_ast.Decl)):
+                continue
+
+            # assert node.coord, "node.coord is None"
+            lineno = node.coord.line
+
+            raw_doc: Comment | None = None
+            if lineno in comments:
+                raw_doc = comments.pop(lineno)
+            elif (lineno - 1) in comments:
+                raw_doc = comments.pop(lineno - 1)
+
+            docstring: Docstring | None = None
+
+            if raw_doc:
+                docstring = parse_docstring(raw_doc.text)
+
+            if isinstance(node, c_ast.Typedef):
+                types[node.name] = DocType(node.name, ast_to_decl(node.type, types), docstring, node.quals)
+
+            elif type(node) is c_ast.Decl:  # we dont want the subclasses
+                if isinstance(node.type, c_ast.FuncDecl):
+                    ref = ast_to_decl(node.type, types)
+                    # assert ref.decl is TypeDecl.FUNCTION, "decl is not TypeDecl.FUNCTION"
+                    # assert ref.params is not None, "function typeref does not have parameters"
+                    params: list[FuncParam] = []
+
+                    for param_ref, param in zip(ref.params, node.type.args.params):  # type: ignore[arg-type]
+                        params.append(FuncParam(param.name, param_ref))
+
+                    funcs.append(DocFunc(node.name, params, ref.name, docstring))  # type: ignore[arg-type]
+                else:
+                    global_vars.append(
+                        DocGlobalVar(
+                            node.name,
+                            ast_to_decl(node.type, types),
+                            docstring,
+                            node.quals,
+                        ),
+                    )
+
+        macros: list[DocMacro] = []
+
+        for macro in macros_list:
+            match = _DEFINE.match(macro.text)
+
+            if not match:
+                continue
+
+            lineno = macro.line_number
+
+            raw_doc = None
+
+            if lineno in comments:
+                raw_doc = comments.pop(lineno)
+            elif (lineno - 1) in comments:
+                raw_doc = comments.pop(lineno - 1)
+
+            docstring = parse_docstring(raw_doc.text) if raw_doc else None
+            macros.append(DocMacro(match.group(1).rstrip(" "), match.group(2) or None, docstring))
+
+        return CodeDoc(macros, funcs, global_vars, types)
+
+    # def get_templates_dir(self, handler: str | None = None) -> Path:
+    #     return Path.cwd()
+
+    def render(self, data: CodeDoc, config: Mapping[str, Any]) -> str:  # noqa: ARG002
         """Render a template using provided data and configuration options.
 
         Parameters:
@@ -73,13 +581,78 @@ class CHandler(BaseHandler):
         Returns:
             The rendered template as HTML.
         """
-        # final_config = {**self.default_config, **config}
-        # heading_level = final_config["heading_level"]
-        # template = self.env.get_template(f"{data...}.html")
-        # return template.render(
-        #     **{"config": final_config, data...: data, "heading_level": heading_level, "root": True},
-        # )
-        raise PluginError("Implement me!")
+        html = StringIO()
+
+        for macro in data.macros:
+            html.write(f"""
+<div>
+    <div>Macro:
+        <code>{macro.name}</code>
+        <code>{macro.content or ""}</code>
+        <p>{desc(macro.doc)}</p>
+    </div>
+</div>""")
+
+        for tp in data.typedefs.values():
+            html.write(f"""
+<div>
+    <div id="type-{tp.name}">Typedef:
+        <code>{tp.name}</code>
+        <code>{tp_ref_to_str(tp.tp, tp.name)}</code>
+        <p>{desc(tp.doc)}</p>
+    </div>
+</div>""")
+
+        for var in data.global_vars:
+            html.write(f"""
+<div>
+    <div>Global:
+        {lookup_type_html(data, var.tp)}
+        <code>{var.name}</code>
+        <p>{desc(var.doc)}</p>
+    </div>
+</div>""")
+
+        for func in data.functions:
+            params: list[str] = []
+
+            for param in func.args:
+                params.append(lookup_type_html(data, param.tp, name=param.name) + f"<code>{param.name}</code>")
+
+            table = StringIO()
+
+            if func.doc and func.doc.params:
+                table.write("""
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Type</th>
+        <th>Description</th>
+    </tr>
+""")
+                for doc, param in zip(func.doc.params, func.args):
+                    in_out = "" if doc.in_out == InOut.UNSPECIFIED else ("[in]" if doc.in_out == InOut.IN else "[out]")
+                    table.write(f"""
+<tr>
+    <td>{doc.name}</td>
+    <td>{lookup_type_html(data, param.tp, name=param.name)}{in_out}</td>
+    <td>{doc.desc}</td>
+</tr>
+""")
+                table.write("</table>")
+
+            html.write(f"""
+<div>
+    <div>Function:
+        {lookup_type_html(data, func.ret)}
+        <code>{func.name}</code>
+        ({', '.join(params) if params else 'void'})
+        <p>{desc(func.doc)}</p>
+        {table.getvalue()}
+    </div>
+</div>""")
+
+        return html.getvalue()
 
     def update_env(self, md: Markdown, config: dict) -> None:
         """Update the Jinja environment with any custom settings/filters/options for this handler.
